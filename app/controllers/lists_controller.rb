721311348587d7ba5126417d77d1c0c324178c0b
Woo Jin Park <woojinp@gmail.com>
2013-04-18 12:53:12 -0700

    updated css

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,7 +3,8 @@ class ListsController < ApplicationController
 	before_filter :authenticate_user!, :only => [:home]
 
 	def home
-		@users = User.all
+		client = Foursquare2::Client.new(:client_id => '4B5MERQAYMRB5F4I535NEBDZRNN24UIUAUFSFNKWIXACNLUX', :client_secret => '0VLPK4IGM5IXJ245JTEOF5ARZSKUBU2ILZISYBSZB4RSMPWU')
+		@venues = client.trending_venues('37.7750,-122.4183',{:limit => 11}).venues
 	end
 
 end
